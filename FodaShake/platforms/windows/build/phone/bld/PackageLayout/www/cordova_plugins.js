bcb5cb8780ff6484394e30948d371cd1e9c3bebe
tripeh <squall@outlook.com>
2015-08-08 18:14:39 -0300

    Initial comit

---
+++ FodaShake/platforms/windows/build/phone/bld/PackageLayout/www/cordova_plugins.js
@@ -0,0 +1,69 @@
+﻿cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-device-orientation/www/CompassError.js",
+        "id": "cordova-plugin-device-orientation.CompassError",
+        "clobbers": [
+            "CompassError"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-device-orientation/www/CompassHeading.js",
+        "id": "cordova-plugin-device-orientation.CompassHeading",
+        "clobbers": [
+            "CompassHeading"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-device-orientation/www/compass.js",
+        "id": "cordova-plugin-device-orientation.compass",
+        "clobbers": [
+            "navigator.compass"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-device-orientation/src/windows/CompassProxy.js",
+        "id": "cordova-plugin-device-orientation.CompassProxy",
+        "merges": [
+            ""
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-shake/www/shake.js",
+        "id": "cordova-plugin-shake.Shake",
+        "clobbers": [
+            "shake"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.device-motion/www/Acceleration.js",
+        "id": "org.apache.cordova.device-motion.Acceleration",
+        "clobbers": [
+            "Acceleration"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.device-motion/www/accelerometer.js",
+        "id": "org.apache.cordova.device-motion.accelerometer",
+        "clobbers": [
+            "navigator.accelerometer"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.device-motion/src/windows/AccelerometerProxy.js",
+        "id": "org.apache.cordova.device-motion.AccelerometerProxy",
+        "merges": [
+            ""
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-device-orientation": "1.0.0",
+    "cordova-plugin-shake": "0.5.1",
+    "cordova-plugin-whitelist": "1.0.0",
+    "org.apache.cordova.device-motion": "0.2.11"
+}
+// BOTTOM OF METADATA
+});
